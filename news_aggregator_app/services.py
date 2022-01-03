d501a88631812cc3054e8acacfe367f08d126966
RamishUrRehman007 <ramish534@outlook.com>
2022-01-04 00:14:14 +0500

    Updated code

--- news_aggregator_app/services.py
+++ news_aggregator_app/services.py
@@ -1,5 +1,7 @@
 from typing import Dict, List
 
+from rest_framework import serializers
+
 from news_aggregator_app.third_party_integrations import thenews
 
 from .third_party_integrations import (reddit,
@@ -7,18 +9,31 @@ from .third_party_integrations import (reddit,
     )
 
 from .serializers import (NewsSerializer,
-    NewsQuerySerializer    
+    NewsQuerySerializer
     )
+
+from .models import (NewsQuery,
+    News
+    )
+
+import datetime
 
 def listNews() -> List[Dict]:
     return reddit.reddit()
 
 def searchNews(query:str) -> List[Dict]:
+    query_id = getQueryByName(query)
+    print(query_id)
+    if query_id:
+        if checkTime(query_id['created_at']) <= 2:
+            return getNewsByQuery(query_id['id'])
+    
     result_thenews = thenews.thenews(query=query)
     result_reddit = reddit.reddit(query=query)
     result_reddit.extend(result_thenews)
     print(createNews(query, result_reddit))
     return result_reddit
+
 
 def createNews(query:str, data:List[Dict]) -> List[Dict]:
     result = []
@@ -34,3 +49,30 @@ def createNews(query:str, data:List[Dict]) -> List[Dict]:
                 result.append(news_serializer.data)
     
     return result
+
+def getQueryByName(query:str) -> int:
+    try:
+        newsquery = NewsQuery.objects.get(query=query)
+        serializer = NewsQuerySerializer(newsquery, many=False)
+        return serializer.data
+    except:
+        return None
+
+def getNewsByQuery(query_id:int) -> int:
+    news = News.objects.filter(query_id=query_id)
+    data_list = []
+
+    for data in news.all():
+        data_list.append(
+            {
+                'link' : data.link,
+                'headline' : data.headline,
+                'source' : data.source
+            }
+        )
+    return data_list
+
+def checkTime(date):
+    today = datetime.datetime.now()
+    DD = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ')
+    return abs((today - DD).days)
