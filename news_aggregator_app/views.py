5ae7b4c51348dd081ae59f4ef34e62d0fc1d70a3
RamishUrRehman007 <ramish534@outlook.com>
2022-01-03 11:00:44 +0500

    Coded a basic APIVIEW for listNews

--- news_aggregator_app/views.py
+++ news_aggregator_app/views.py
@@ -5,3 +5,14 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+@api_view(['GET'])
+def listNews(request):
+    api_response = [
+        {
+        "headline": "Human organs can be stored for three times as long in major breakthrough for transplants",  
+        "link": "https://www.telegraph.co.uk/science/2019/09/09/human-organs-can-stored-three-times-long-major-breakthrough/",
+        "source": "reddit"
+        }
+    ]
+
+    return Response(api_response)
