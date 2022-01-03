c9e50eec01f97c248ecb8377d1c236c5756ba001
RamishUrRehman007 <ramish534@outlook.com>
2022-01-03 14:49:00 +0500

    Created new model for query and relationships

--- news_aggregator_app/models.py
+++ news_aggregator_app/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class NewsQuery(models.Model):
+    query = models.CharField(max_length=250, unique=True, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.query
 
 class News(models.Model):
     link = models.CharField(max_length=250, null=False, blank=False)
     headline = models.TextField()
     source = models.CharField(max_length=250, null=False, blank=False)
-    query = models.CharField(max_length=250, unique=True, null=False, blank=False)
+    query = models.ForeignKey(NewsQuery, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
 
     def __str__(self) -> str:
