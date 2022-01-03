657cb477ef3a6af034d4d1139576ac14e90554d8
RamishUrRehman007 <ramish534@outlook.com>
2022-01-03 11:38:08 +0500

    Added query option to search

--- news_aggregator_app/views.py
+++ news_aggregator_app/views.py
@@ -7,12 +7,25 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def listNews(request):
-    api_response = [
-        {
-        "headline": "Human organs can be stored for three times as long in major breakthrough for transplants",  
-        "link": "https://www.telegraph.co.uk/science/2019/09/09/human-organs-can-stored-three-times-long-major-breakthrough/",
-        "source": "reddit"
-        }
-    ]
+
+    query = request.query_params.get('query', None)
+    if query is not None:
+        # queryset = queryset.filter(category=category)
+        api_response = [
+            {
+            "headline": "Human organs can be stored for three times as long in major breakthrough for transplants",  
+            "link": "https://www.telegraph.co.uk/science/2019/09/09/human-organs-can-stored-three-times-long-major-breakthrough/",
+            "source": query
+            }
+        ]
+    else:
+        api_response = [
+            {
+            "headline": "Human organs can be stored for three times as long in major breakthrough for transplants",  
+            "link": "https://www.telegraph.co.uk/science/2019/09/09/human-organs-can-stored-three-times-long-major-breakthrough/",
+            "source": "reddit"
+            }
+        ]
+    
 
     return Response(api_response)
