87e3065ecb159258f5d8e2d743a9766e39078075
RamishUrRehman007 <ramish534@outlook.com>
2022-01-03 14:30:27 +0500

    Changed property of query field

--- news_aggregator_app/models.py
+++ news_aggregator_app/models.py
@@ -6,7 +6,7 @@ class News(models.Model):
     link = models.CharField(max_length=250, null=False, blank=False)
     headline = models.TextField()
     source = models.CharField(max_length=250, null=False, blank=False)
-    query = models.CharField(max_length=250, null=False, blank=False)
+    query = models.CharField(max_length=250, unique=True, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
 
     def __str__(self) -> str:
