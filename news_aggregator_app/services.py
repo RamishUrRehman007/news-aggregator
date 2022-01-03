c00778926b4a949ac8019611de64344327a4a950
RamishUrRehman007 <ramish534@outlook.com>
2022-01-03 15:11:59 +0500

    Storing the query into db

--- news_aggregator_app/services.py
+++ news_aggregator_app/services.py
@@ -4,7 +4,11 @@ from news_aggregator_app.third_party_integrations import thenews
 
 from .third_party_integrations import (reddit,
     thenews
-)
+    )
+
+from .serializers import (NewsSerializer,
+    NewsQuerySerializer    
+    )
 
 def listNews() -> List[Dict]:
     return reddit.reddit()
@@ -13,4 +17,13 @@ def searchNews(query:str) -> List[Dict]:
     result_thenews = thenews.thenews(query=query)
     result_reddit = reddit.reddit(query=query)
     result_reddit.extend(result_thenews)
+    print(createNews(query, result_thenews))
     return result_reddit
+
+def createNews(query:str, data:List[Dict]) -> List[Dict]:
+    query_serializer = NewsQuerySerializer(data = {'query' : query})
+    
+    if query_serializer.is_valid():
+        query_serializer.save()
+    
+    return query_serializer.data
