d53c2c40ce14164c30df3b9c3178823a30abe61b
RamishUrRehman007 <ramish534@outlook.com>
2022-01-03 14:28:25 +0500

    Coded basic model for storing news

--- news_aggregator_app/models.py
+++ news_aggregator_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class News(models.Model):
+    link = models.CharField(max_length=250, null=False, blank=False)
+    headline = models.TextField()
+    source = models.CharField(max_length=250, null=False, blank=False)
+    query = models.CharField(max_length=250, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.headline
