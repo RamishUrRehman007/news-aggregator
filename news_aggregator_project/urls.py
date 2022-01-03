0571e1c842ca62f8b341280ff8e222a67ad0ec03
RamishUrRehman007 <ramish534@outlook.com>
2022-01-04 04:21:49 +0500

    Basic Swagger UI

--- news_aggregator_project/urls.py
+++ news_aggregator_project/urls.py
@@ -23,9 +23,9 @@ from drf_yasg2 import openapi
 
 schema_view = get_schema_view(
       openapi.Info(
-         title="Snippets API",
+         title="News Aggregator API",
          default_version='v1',
-         description="Test description",
+         description="News Aggregator",
          terms_of_service="https://www.google.com/policies/terms/",
          contact=openapi.Contact(email="contact@snippets.local"),
          license=openapi.License(name="BSD License"),
