7b2ecff65b20f949cbc3860dfe682d12b5a4048a
RamishUrRehman007 <ramish534@outlook.com>
2022-01-04 02:23:37 +0500

    Added a model in serializer

--- news_aggregator_app/serializers.py
+++ news_aggregator_app/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
 from .models import ( News,
-    NewsQuery
+    NewsQuery,
+    Favourite
     )
 
 class NewsQuerySerializer(serializers.ModelSerializer):
@@ -17,3 +18,8 @@ class GetNewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = News
         fields = ('headline', 'source')
+
+class FavouriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Favourite
+        fields = ('__all__')
